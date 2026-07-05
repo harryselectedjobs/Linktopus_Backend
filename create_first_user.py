236d789fb015a8b89872915806ff9c9434e71533
rahulkrguptave <rkgupta@virtualemployee.com>
2026-07-05 18:47:20 +0530

    jkfhsdjf

--- create_first_user.py
+++ create_first_user.py
@@ -1,4 +1,4 @@
 from services.user_service import create_user
 
-result = create_user("rahul@test.com", "Eightmay2000")
-print(result)
+# result = create_user("rahul@test.com", "Eightmay2000")
+# print(result)
