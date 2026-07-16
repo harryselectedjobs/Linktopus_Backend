040b6fcf16a52a5aa07e38bd923051d02972cd95
rahulkrguptave <rkgupta@virtualemployee.com>
2026-07-16 17:29:30 +0530

    automation router configured

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from routers.auth_router import router as auth_router
 from routers.share_post_router import router as share_post_router
 from routers.webhooks import router as wenhook_router
+from routers.linkedin_recruiter_automation_router import router as automation_router
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -17,6 +18,8 @@ app.add_middleware(
 app.include_router(auth_router)
 app.include_router(share_post_router)
 app.include_router(wenhook_router)
+app.include_router(automation_router)
+
 
 
 if __name__ == "__main__":
