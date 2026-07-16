5128b84d70b5931a5b31758b4cf7ae4c79b5ad03
rahulkrguptave <rkgupta@virtualemployee.com>
2026-07-16 17:43:00 +0530

    solved wromng indentation

--- models/linkedin_chat.py
+++ models/linkedin_chat.py
@@ -2,9 +2,9 @@ from typing import Optional
 from pydantic import BaseModel
 
 
-    class LinkedInChatRequest(BaseModel):
-        account_id: str = "Ryi4vdHxS8O4ChMtypDbLQ"
-        text: str
-        attendees_ids: str
-        linkedin_api: str = "recruiter"
-        linkedin_inmail: bool = True
+class LinkedInChatRequest(BaseModel):
+    account_id: str = "Ryi4vdHxS8O4ChMtypDbLQ"
+    text: str
+    attendees_ids: str
+    linkedin_api: str = "recruiter"
+    linkedin_inmail: bool = True
