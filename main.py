02d6cc97f97cef7cfed6a0887284b6098bf706c8
rahulkrguptave <rkgupta@virtualemployee.com>
2026-07-22 20:05:46 +0530

    Trigger deployment

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ app.include_router(automation_router)
 app.include_router(calendar_router)
 
 
-
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=1802)
